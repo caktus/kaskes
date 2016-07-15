efaf0b5ef384a3145067cb18fd7f64baabbb2fc2
Neil Ashton <neil.m.ashton@gmail.com>
2016-07-15 08:56:30 -0400

    Adding name parameter

--- Handler/ChatSocket.hs
+++ Handler/ChatSocket.hs
@@ -14,24 +14,36 @@ import Data.Aeson
 data SendMessage = SendMessage
   { name    :: Text
   , message :: Text
+  , type_    :: Text
   }
 
 instance ToJSON SendMessage where
   toJSON SendMessage {..} = object
     [ "name"    .= name
     , "message" .= message
+    , "type"    .= type_
     ]
 
 chatSocket :: WebSocketsT Handler ()
 chatSocket = do
+  sendTextData ("Welcome to Kaskes. Please enter your name." :: Text)
+  name <- receiveData
+  sendTextData $ encode $ SendMessage {
+      name = "server"
+    , message = name <> " has joined"
+    , type_ = "SERVER_MESSAGE"
+    }
+
   writeChan <- channel <$> getYesod
   readChan <- atomically $ dupTChan writeChan
+
   race_
     (forever $ do
       text <- atomically $ readTChan readChan
       let msg = SendMessage {
-        name = "arb"
+        name = name
       , message = text
+      , type_ = "NEW_MESSAGE"
       }
       sendTextData $ encode $ msg)
     (sourceWS $$ mapM_C (\msg ->
