89767965ae1b720e2709ad2b8268aa8661c16b10
Neil Ashton <neil.m.ashton@gmail.com>
2016-07-15 08:45:50 -0400

    Transmitting (echoed) messages in JSON format

--- Handler/ChatSocket.hs
+++ Handler/ChatSocket.hs
@@ -9,13 +9,31 @@ import Import
 import Yesod.Core
 import Yesod.WebSockets
 import Data.Text (Text)
+import Data.Aeson
+
+data SendMessage = SendMessage
+  { name    :: Text
+  , message :: Text
+  }
+
+instance ToJSON SendMessage where
+  toJSON SendMessage {..} = object
+    [ "name"    .= name
+    , "message" .= message
+    ]
 
 chatSocket :: WebSocketsT Handler ()
 chatSocket = do
   writeChan <- channel <$> getYesod
   readChan <- atomically $ dupTChan writeChan
   race_
-    (forever $ atomically (readTChan readChan) >>= sendTextData)
+    (forever $ do
+      text <- atomically $ readTChan readChan
+      let msg = SendMessage {
+        name = "arb"
+      , message = text
+      }
+      sendTextData $ encode $ msg)
     (sourceWS $$ mapM_C (\msg ->
       atomically $ writeTChan writeChan msg))
 
@@ -23,4 +41,4 @@ chatSocket = do
 getChatSocketR :: Handler Html
 getChatSocketR = do
   webSockets chatSocket
-  defaultLayout [whamlet|No dice, chief!|]
+  defaultLayout ""
